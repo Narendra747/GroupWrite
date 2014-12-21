9ae3a512512c9100caef5c0688f0f75b37af57ea
Narendra Patwardhan <detectiven747@gmail.com>
2014-12-21 20:45:24 +0530

    Update GroupWrite.h

--- GroupWrite.h
+++ GroupWrite.h
@@ -10,6 +10,7 @@ class GroupWrite
     void set(byte value1,byte value2,byte value3,byte value4);
   private:
     byte _pin1,_pin2,_pin3,_pin4,_value1,_value2,_value3,_value4;
+    int k;
 };
 
 #endif
