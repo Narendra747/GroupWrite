37d3c95f82144bf0e3c33a8227463a173584b0d0
Narendra Patwardhan <detectiven747@gmail.com>
2014-12-21 20:38:58 +0530

    Create GroupWrite.h

---
+++ GroupWrite.h
@@ -0,0 +1,15 @@
+#ifndef GroupWrite_h
+#define GroupWrite_h
+
+#include "Arduino.h"
+
+class GroupWrite
+{
+  public:
+    void group(byte pin1,byte pin2,byte pin3,byte pin4);
+    void set(byte value1,byte value2,byte value3,byte value4);
+  private:
+    byte _pin1,_pin2,_pin3,_pin4,_value1,_value2,_value3,_value4;
+};
+
+#endif
